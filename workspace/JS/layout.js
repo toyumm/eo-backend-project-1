dbc699c7e78aba1f877413da18c41db03be40b1c
Sangee Yoon <sk41442457@gmail.com>
2025-12-24 17:38:24 +0900

    마무리 작업

--- workspace/JS/layout.js
+++ workspace/JS/layout.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     fetch("../html/head.html")
         .then(function (res) {
             return res.text();
         })
         .then(function (html) {
-            document.getElementById("header-area").innerHTML = html;
+            const headerArea = document.getElementById("header-area");
+            if (headerArea) {
+                headerArea.innerHTML = html;
+            }
         });
 
     fetch("../html/foot.html")
@@ -12,6 +16,9 @@ document.addEventListener("DOMContentLoaded", function () {
             return res.text();
         })
         .then(function (html) {
-            document.getElementById("footer-area").innerHTML = html;
+            const footerArea = document.getElementById("footer-area");
+            if (footerArea) {
+                footerArea.innerHTML = html;
+            }
         });
 });
